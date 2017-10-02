4a1d7a04aceb059af24495499793d5897eb71531
Rick Koster <ngf.koster@outlook.com>
2017-10-02 10:25:25 +0200

    feat(ng-store): allow custom store dependencies

--- packages/ng-store/src/ng-store.module.ts
+++ packages/ng-store/src/ng-store.module.ts
@@ -1,27 +1,28 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
-import { Reducer, StoreConstructor } from '@ngfk/store';
+import { Store } from '@ngfk/store';
+
+export type StoreConstructor<State> = {
+    new (...args: any[]): Store<State, any>;
+};
 
 @NgModule({})
 export class NgStoreModule {
     public static forRoot<
-        State,
-        Store extends StoreConstructor<State>
+        Store extends StoreConstructor<State>,
+        State
     >(options: {
         readonly store: Store;
-        readonly reducer: Reducer<State>;
-        readonly initialState?: State;
+        readonly deps?: any[];
     }): ModuleWithProviders {
         return {
             ngModule: NgStoreModule,
             providers: [
                 {
                     provide: options.store,
-                    useFactory: function storeFactory() {
-                        return new options.store(
-                            options.reducer,
-                            options.initialState
-                        );
-                    }
+                    useFactory: function storeFactory(...deps: any[]) {
+                        return new options.store(...deps);
+                    },
+                    deps: options.deps
                 }
             ]
         };
