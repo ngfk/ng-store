a733eb2cdc686525d50ede3fc6cf9d8181f808c3
Rick Koster <ngf.koster@outlook.com>
2017-10-02 10:46:36 +0200

    docs(ng-store): update ng-store example

--- examples/ng-store/src/app/app.module.ts
+++ examples/ng-store/src/app/app.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { NgStoreModule } from '@ngfk/ng-store';
 
 import { StoreService } from '../services/store.service';
-import { reducer } from '../state';
 
 @NgModule({
     imports: [
         NgStoreModule.forRoot({
-            store: StoreService,
-            reducer: reducer
+            store: StoreService
         })
     ]
 })
