f706685d7270d0a043616adb74da6d17e07eaff4
Rick Koster <ngf.koster@outlook.com>
2017-10-02 22:08:34 +0200

    feat(ng-store): use IStore contract as store & provide factory options using DI

--- packages/ng-store/src/ng-store.module.ts
+++ packages/ng-store/src/ng-store.module.ts
@@ -1,28 +1,33 @@
-import { ModuleWithProviders, NgModule } from '@angular/core';
-import { Store } from '@ngfk/store';
+import { InjectionToken, ModuleWithProviders, NgModule } from '@angular/core';
+import { IStore } from '@ngfk/store';
 
-export type StoreConstructor<State> = {
-    new (...args: any[]): Store<State, any>;
-};
+const OPTIONS_TOKEN = new InjectionToken('NgStoreOptions');
+
+export class NgStoreOptions<S, A, T extends IStore<S, A>> {
+    store: { new (...d: any[]): T };
+    deps?: any[];
+}
+
+export function storeFactory(options: NgStoreOptions<any, any, any>) {
+    return new options.store(...(options.deps || []));
+}
 
 @NgModule({})
 export class NgStoreModule {
-    public static forRoot<
-        Store extends StoreConstructor<State>,
-        State
-    >(options: {
-        readonly store: Store;
-        readonly deps?: any[];
-    }): ModuleWithProviders {
+    public static forRoot<S, A, T extends IStore<S, A>>(
+        options: NgStoreOptions<S, A, T>
+    ): ModuleWithProviders {
         return {
             ngModule: NgStoreModule,
             providers: [
                 {
+                    provide: OPTIONS_TOKEN,
+                    useValue: options
+                },
+                {
                     provide: options.store,
-                    useFactory: function storeFactory(...deps: any[]) {
-                        return new options.store(...deps);
-                    },
-                    deps: options.deps
+                    useFactory: storeFactory,
+                    deps: [OPTIONS_TOKEN]
                 }
             ]
         };
